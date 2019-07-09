61db2158894630c35d0f98b9a32026aa518a9be8
Slamdoom <slamdoom@slam.doom>
2019-07-09 10:09:14 +0800

    add xauth init

---
+++ xauth_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# only need to run the following auth once
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
